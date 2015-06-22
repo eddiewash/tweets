b6bf33da3fbe068b36e5704d322fb9776ec29ee0
Eddie <edward.washington10@gmail.com>
2015-06-21 22:01:20 -0400

    twitter bundled

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = '9BEXBrKYSuM9RwQROeJA'
+  config.consumer_secret = 'lBCKDUiu7fCZRzppn5lPXiZzigRsO1mspblLbm7oaD0'
+  config.access_token = '731847589-62deO4RuhrqRBeBKkAIdYFJMhhYN68NRUTQkwTjU'
+  config.access_token_secret = '0TeVtXgmFoSmFjTYsPU2PeRadxgXToVcFteDsObO1cItv'
+end
